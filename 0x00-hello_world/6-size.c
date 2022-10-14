ed9c6251d75d05a714279226ba848a9078805249
LevOgude <wottiz001@students.uonbi.ac.ke>
2022-10-14 09:36:25 -0700

    task6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+ze of an int: %i byte(s)\n", sizeof(int))
+return (0);
+};/**
+ * main- Entry
+ * Return: Always 0
+ */
+int main(void)
+{
+	printf("Size of a char: %i byte(s)\n", sizeof(char));
+i
